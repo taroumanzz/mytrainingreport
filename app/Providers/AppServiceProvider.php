46a0aa9683f10cb4b59e1b82ac162f211827301c
tarou <tarou@TacBook-Air.local>
2023-10-07 13:13:15 +0900

    make it better

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,9 +21,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
-
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
-        }
     }
 }
